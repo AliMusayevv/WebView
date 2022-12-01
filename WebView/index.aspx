00025841370fb835003d6f1f82fde6e454560cfa
AliMusayevv <emusayev001@gmail.com>
2022-12-01 00:53:35 -0800

    A

--- WebView/index.aspx
+++ WebView/index.aspx
@@ -4,13 +4,13 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title> WebView</title>
     <link href="Css/style.css" rel="stylesheet" />
 </head>
 <body>
     <form id="form1" runat="server">
         <div class="textbox">
-            <asp:Label ID="Label1" runat="server" Text="Category Name:" class="LblCatName"></asp:Label><asp:TextBox ID="TxtSearch" runat="server" class="TxtSearch"></asp:TextBox>
+            <asp:Label ID="Label1" runat="server" Text="Category Name:" class="LblCatName"></asp:Label><asp:TextBox ID="TxtSearch" runat="server" class="TxtSearch" OnTextChanged="TxtSearch_TextChanged"></asp:TextBox>
         </div>
         <div>
             <asp:Button ID="BtnSearch" runat="server" class="button"  Text="Search" OnClick="BtnSearch_Click" />
@@ -21,7 +21,8 @@
         </div>
         <div>
             <asp:GridView ID="GrdResult" runat="server" CssClass="mydatagrid" PagerStyle-CssClass="pager"
- HeaderStyle-CssClass="header" RowStyle-CssClass="rows" AllowPaging="True" ></asp:GridView>
+                HeaderStyle-CssClass="header" RowStyle-CssClass="rows" AllowPaging="True" OnPageIndexChanging="GrdResult_PageIndexChanging" PageSize="6">
+            </asp:GridView>
         </div>
         
     </form>
--- WebView/index.aspx.cs
+++ WebView/index.aspx.cs
@@ -12,6 +12,7 @@ namespace WebView
 {
     public partial class index : System.Web.UI.Page
     {
+        bool TextChanged = false;
         SqlConnection conn = new SqlConnection(DataSource.DS);
         SqlCommand cmd= new SqlCommand();
         string query;
@@ -19,32 +20,65 @@ namespace WebView
         DataSet DS=new DataSet();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
+            
+            LblResult.Visible= false;
         }
 
         protected void BtnSearch_Click(object sender, EventArgs e)
         {
-            if (conn.State != ConnectionState.Open)
-            {
-                conn.Open();
-            }
-            query = "select*from CatProView where CategoryName='" + TxtSearch.Text + "'";
-            cmd =new SqlCommand(query,conn);
-            SqlDataAdapter DA = new SqlDataAdapter(cmd);
-            DA.Fill(DS);
-            GrdResult.DataSource = DS.Tables[0].DefaultView;
-             GrdResult.DataBind();
-            GrdResult.Visible= true;
-            LblResult.Visible = false;
-            if (DS.Tables[0].Rows.Count == 0)
-            {
-                LblResult.Text = "There is not data such as this CategoryName";
-                GrdResult.Visible = false;
-                LblResult.Visible = true;
-            }
-
+            this.MetGrd();
 
         }
 
+        private void MetGrd()
+        {
+            if(TextChanged)
+            {
+                Session["data"] = null;
+            }
+
+
+            if (Session["data"]==null)
+            {
+
+            
+              if (conn.State != ConnectionState.Open)
+              {
+                conn.Open();
+              }
+               query = "select*from CatProView where CategoryName='" + TxtSearch.Text + "'";
+               cmd = new SqlCommand(query, conn);
+               SqlDataAdapter DA = new SqlDataAdapter(cmd);
+               DA.Fill(DS);
+               Session["data"] = DS.Tables[0].DefaultView;
+               GrdResult.DataSource = Session["data"]; 
+           
+               if (DS.Tables[0].Rows.Count == 0)
+               {
+                LblResult.Text = "There is no data such as this CategoryName";
+                GrdResult.Visible = false;
+                LblResult.Visible = true;
+               }
+                conn.Close();
+
+            }
+
+            GrdResult.DataBind();
+            GrdResult.Visible = true;
+            LblResult.Visible = false;
+
+        }
+
+        protected void GrdResult_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GrdResult.PageIndex = e.NewPageIndex;
+            this.MetGrd();
+        }
+
+        protected void TxtSearch_TextChanged(object sender, EventArgs e)
+        {
+            TextChanged = true;
+        }
     }
 }
